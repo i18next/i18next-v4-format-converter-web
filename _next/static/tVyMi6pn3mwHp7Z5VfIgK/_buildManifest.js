6946213f1b0ef6f1f9169f39149e9319d9a356d1
Adriano Raiano <adriano@raiano.ch>
2021-09-17 20:01:59 +0200

    Updates

--- _next/static/tVyMi6pn3mwHp7Z5VfIgK/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-71c04e7a81b5fb26b8f8.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
